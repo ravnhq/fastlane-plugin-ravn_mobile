e320cdab3cac8fcb207ff7808ec974892ad0f985
Kevin DC <kevindelcastillo@ravn.co>
2023-12-07 09:25:17 -0500

    chore: Remove ravn_mobile_action_spec.rb

--- spec/ravn_mobile_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::RavnMobileAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The ravn_mobile plugin is working!")
-
-      Fastlane::Actions::RavnMobileAction.run(nil)
-    end
-  end
-end
