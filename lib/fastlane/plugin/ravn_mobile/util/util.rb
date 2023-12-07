69cf5a48865052ea72fba960ef1712f98a06faf8
Kevin DC <kevindelcastillo@ravn.co>
2023-12-07 10:40:23 -0500

    chore: Change `is_a?` to `kind_of?`

--- lib/fastlane/plugin/ravn_mobile/util/util.rb
+++ lib/fastlane/plugin/ravn_mobile/util/util.rb
@@ -1,7 +1,7 @@
 # @param [Object] obj
 # @return [TrueClass, FalseClass]
 def blank?(obj)
-  return obj.strip.empty? if obj.is_a?(String)
+  return obj.strip.empty? if obj.kind_of?(String)
 
   obj.respond_to?(:empty?) ? obj.empty? : !obj
 end
